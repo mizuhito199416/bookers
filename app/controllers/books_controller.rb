2a6a4548be05fd9d6b37ee04975caab2614e04c2
mizuhito199416 <139484408+mizuhito199416@users.noreply.github.com>
2023-09-03 15:30:42 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,52 @@
 class BooksController < ApplicationController
+
+
   def index
+   @books = Book.all
+   @book = Book.new
+
   end
 
+  def create
+   @book = Book.new(book_params)
+   if @book.save
+    redirect_to book_path(@book.id)
+   else
+     @books = Book.all
+     render :index
+   end
+  end
+
+
   def show
+   @book = Book.find(params[:id])
   end
 
   def edit
+   @book = Book.find(params[:id])
   end
 
-  def new
+  def update
+   @book = Book.find(params[:id])
+   if @book.update(book_params)
+    redirect_to book_path(book.id)
+   else
+    @book = Book.find
+    render :ebit
+   end
   end
+
+  def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to '/books'
+  end
+
+
+
+  private
+  def book_params
+   params.require(:book).permit(:title, :body)
+  end
+
 end
