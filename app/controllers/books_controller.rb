a5e814440f28ccc30327974fea9193a8fd03e43b
mizuhito199416 <139484408+mizuhito199416@users.noreply.github.com>
2023-09-04 15:12:59 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,10 +29,9 @@ class BooksController < ApplicationController
   def update
    @book = Book.find(params[:id])
    if @book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
    else
-    @book = Book.find
-    render :ebit
+    render 'books/edit'
    end
   end
 
